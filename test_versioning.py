c261dac6a098db433c369d4e392c0f14397d8474
HenriquePvAr <henriquepaiva128@gmail.com>
2026-09-21 11:15:53 -0400

    release: 0.9.1-beta.9

--- test_versioning.py
+++ test_versioning.py
@@ -5,7 +5,7 @@ import app_version
 
 def test_canonical_versions_are_current():
     assert app_version.PRODUCT_VERSION == "0.9.0"
-    assert app_version.BUILD_VERSION == "0.9.1-beta.8"
+    assert app_version.BUILD_VERSION == "0.9.1-beta.9"
     assert app_version.DISPLAY_VERSION == app_version.BUILD_VERSION
 
 
